9b2ee876a3f4f4180ecc5f638596ec6d4e8783d4
yxj <564197835@qq.com>
2023-02-02 09:44:55 +0800

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_state_download_button/controller.dart';
 
 void main() {
   runApp(
@@ -20,137 +21,25 @@ class MultiStateDownloadButton extends StatefulWidget {
 }
 
 class _MultiStateDownloadButtonState extends State<MultiStateDownloadButton> {
-  late final DownloadController _downloadController;
-
-  @override
-  void initState() {
-    super.initState();
-    _downloadController = SimulatedDownloadController();
-  }
+  final DownloadController _downloadController = SimulatedDownloadController();
 
   @override
   Widget build(BuildContext context) {
-    final downloadController = _downloadController;
     return SizedBox(
       width: 96,
       child: AnimatedBuilder(
-        animation: downloadController,
+        animation: _downloadController,
         builder: (context, child) {
           return DownloadButton(
-            status: downloadController.downloadStatus,
-            downloadProgress: downloadController.progress,
-            onDownload: downloadController.startDownload,
-            onCancel: downloadController.stopDownload,
-            onOpen: downloadController.openDownload,
+            status: _downloadController.downloadStatus,
+            downloadProgress: _downloadController.progress,
+            onDownload: _downloadController.startDownload,
+            onCancel: _downloadController.stopDownload,
+            onOpen: _downloadController.openDownload,
           );
         },
       ),
     );
-  }
-}
-
-enum DownloadStatus {
-  notDownloaded,
-  fetchingDownload,
-  downloading,
-  downloaded,
-}
-
-abstract class DownloadController implements ChangeNotifier {
-  DownloadStatus get downloadStatus;
-  double get progress;
-
-  void startDownload();
-  void stopDownload();
-  void openDownload();
-}
-
-class SimulatedDownloadController extends DownloadController
-    with ChangeNotifier {
-  SimulatedDownloadController(
-      {DownloadStatus downloadStatus = DownloadStatus.notDownloaded,
-      double progress = 0.0})
-      : _downloadStatus = downloadStatus,
-        _progress = progress;
-
-  DownloadStatus _downloadStatus;
-  @override
-  DownloadStatus get downloadStatus => _downloadStatus;
-
-  double _progress;
-  @override
-  double get progress => _progress;
-
-  bool _isDownloading = false;
-
-  @override
-  void startDownload() {
-    if (downloadStatus == DownloadStatus.notDownloaded) {
-      _doSimulatedDownload();
-    }
-  }
-
-  @override
-  void stopDownload() {
-    if (_isDownloading) {
-      _isDownloading = false;
-      _downloadStatus = DownloadStatus.notDownloaded;
-      _progress = 0.0;
-      notifyListeners();
-    }
-  }
-
-  @override
-  void openDownload() {
-    if (downloadStatus == DownloadStatus.downloaded) {
-      debugPrint("open download");
-    }
-  }
-
-  Future<void> _doSimulatedDownload() async {
-    _isDownloading = true;
-    _downloadStatus = DownloadStatus.fetchingDownload;
-    notifyListeners();
-
-    // Wait a second to simulate fetch time.
-    await Future<void>.delayed(const Duration(seconds: 1));
-
-    // If the user chose to cancel the download, stop the simulation.
-    if (!_isDownloading) {
-      return;
-    }
-
-    // Shift to the downloading phase.
-    _downloadStatus = DownloadStatus.downloading;
-    notifyListeners();
-
-    const downloadProgressStops = [0.0, 0.15, 0.45, 0.8, 1.0];
-    for (final stop in downloadProgressStops) {
-      // Wait a second to simulate varying download speeds.
-      await Future<void>.delayed(const Duration(seconds: 1));
-
-      // If the user chose to cancel the download, stop the simulation.
-      if (!_isDownloading) {
-        return;
-      }
-
-      // Update the download progress.
-      _progress = stop;
-      notifyListeners();
-    }
-
-    // Wait a second to simulate a final delay.
-    await Future<void>.delayed(const Duration(seconds: 1));
-
-    // If the user chose to cancel the download, stop the simulation.
-    if (!_isDownloading) {
-      return;
-    }
-
-    // Shift to the downloaded state, completing the simulation.
-    _downloadStatus = DownloadStatus.downloaded;
-    _isDownloading = false;
-    notifyListeners();
   }
 }
 
